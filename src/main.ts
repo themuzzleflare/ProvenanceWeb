06a14a2ca4ccbf7928b51c83f008cea891fcd99b
Paul Tavitian <paul.tavitian@icloud.com>
2021-11-11 21:01:17 +1100

    Updates

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag-next";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueGtag, { property: { id: "UA-122946443-40" } })
+  .mount("#app");
