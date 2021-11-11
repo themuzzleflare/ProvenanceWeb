d9bf52de88988b7d4818e5c6e2826c5f66d1b0f7
Paul Tavitian <paul.tavitian@icloud.com>
2021-11-11 19:27:47 +1100

    Updates

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,26 @@
 // Copyright © 2021 Paul Tavitian
 
 import { createStore } from "vuex";
+import { reactive } from "vue";
+
+type State = {
+  relativeDates: boolean;
+  dateGrouping: boolean;
+};
 
 export default createStore({
-  state: {
+  state: reactive<State>({
     relativeDates: localStorage.relativeDates === "1",
     dateGrouping: localStorage.dateGrouping === "1",
-  },
+  }),
   mutations: {
-    transactionGrouping(state, payload): void {
+    transactionGrouping(state: State, payload: boolean): void {
       localStorage.dateGrouping = payload ? "1" : "0";
       state.dateGrouping = payload;
     },
-    dateStyle(state, payload): void {
+    dateStyle(state: State, payload: boolean): void {
       localStorage.relativeDates = payload ? "1" : "0";
       state.relativeDates = payload;
     },
   },
-  actions: {},
-  modules: {},
 });
