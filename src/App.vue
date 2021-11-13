4b9de599768f93f31fe3c0da9b1d76aec8c0a874
Paul Tavitian <paul.tavitian@icloud.com>
2021-11-13 18:37:37 +1100

    Updaded navigation style

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div id="navbarSupportedContent" class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav nav-tabs me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link" to="/transactions"
               >Transactions</router-link
