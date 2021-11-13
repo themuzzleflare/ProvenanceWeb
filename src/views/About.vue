d3209b46dacb1b75b9b0d41a673950369e0a42df
Paul Tavitian <paul.tavitian@icloud.com>
2021-11-13 17:32:17 +1100

    Added about links

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,16 +7,56 @@
       Developer API to display information about your bank accounts,
       transactions, categories, tags and more.
     </span>
+    <div class="list-group">
+      <a
+        class="list-group-item list-group-item-action"
+        href="https://github.com/themuzzleflare/ProvenanceWeb"
+        target="_blank"
+      >
+        <font-awesome-icon :icon="['fab', 'github']" />
+        <span>GitHub</span>
+      </a>
+      <a
+        class="list-group-item list-group-item-action"
+        href="mailto:feedback@tavitian.cloud"
+        target="_blank"
+      >
+        <font-awesome-icon :icon="['fas', 'envelope']" />
+        <span>Contact Developer</span>
+      </a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGithub);
+library.add(faEnvelope);
+
+@Options({
+  components: { FontAwesomeIcon },
+})
 export default class About extends Vue {}
 </script>
 
 <style lang="scss" scoped>
 .about {
   margin: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.list-group-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
 }
 </style>
